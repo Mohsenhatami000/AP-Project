d7f96f32110b06a517e4b746013f120acc71c6e4
Mohsen_Hatami <mohsenhatami13852006@gmail.com>
2026-06-30 17:51:26 +0330

    feat: add initial LoginPage UI

--- Client/Presentation/MainWindow/mainwindow.cpp
+++ Client/Presentation/MainWindow/mainwindow.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
+#include "../Login/loginpage.h"
 #include "ui_mainwindow.h"
+
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    LoginPage *loginPage = new LoginPage(this);
+    ui->stackedWidget->addWidget(loginPage);
+    ui->stackedWidget->setCurrentIndex(0);
+    ui->stackedWidget->setCurrentWidget(loginPage);
 }
 
 MainWindow::~MainWindow()
